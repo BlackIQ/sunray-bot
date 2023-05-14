3f38c9cd81b5826f1d3b2963ecdb60adf68457a9
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-05-14 11:26:25 +0330

    messages.join(n)

--- bot/controllers/bot/bot.controllers.js
+++ bot/controllers/bot/bot.controllers.js
@@ -1,7 +1,14 @@
 export const START = async (ctx) => {
-  await ctx.reply(
-    "iSunray is a project that say greadings! Issues and stuff: @BlackIQ. You can use /add to be add in the list and /delete to be deleted from the list. For more information enter /help."
-  );
+  const messages = [
+    "iSunray is a project that say greadings! Issues and stuff: @BlackIQ. You can use /add to be add in the list and /delete to be deleted from the list. For more information enter /help.",
+    "- For more information read [this blog](https://blog.amirhossein.info/?p=91)",
+    "",
+    "SunRay is open-source",
+    "- [API source](https://blog.amirhossein.info/?p=91)",
+    "- [Bot source](https://blog.amirhossein.info/?p=91)",
+  ];
+
+  await ctx.replyWithMarkdown(messages.join("\n"));
 };
 
 export const HELP = async (ctx) => {
